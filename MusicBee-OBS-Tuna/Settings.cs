128a8f7aac290edcaa1b0a159bc8f14300487981
Sisk S'jet <sisk.sjet@gmail.com>
2024-08-23 18:35:20 +0200

    🔧: add settings class for storing and loading application settings
    - Provides default port number for the application
    - Includes methods to load and save settings from XML files

---
+++ MusicBee-OBS-Tuna/Settings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Sisk.MusicBee.OBS.Tuna {
+
+    [Serializable]
+    internal class Settings {
+        public const int DEFAULT_PORT = 1608;
+        public int Port { get; set; } = DEFAULT_PORT;
+
+        public static Settings Load(string file) {
+            if (File.Exists(file)) {
+                using (var reader = new StreamReader(file)) {
+                    var serializer = new XmlSerializer(typeof(Settings));
+                    return (Settings)serializer.Deserialize(reader);
+                }
+            }
+
+            return new Settings();
+        }
+
+        public void Save(string file) {
+            using (var writer = new StreamWriter(file)) {
+                var serializer = new XmlSerializer(typeof(Settings));
+                serializer.Serialize(writer, this);
+            }
+        }
+    }
+}
